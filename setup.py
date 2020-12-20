0b64b316667214f311bd31a3e0284c83399c8c5e
Louis RIVIERE <louis@riviere.xyz>
2020-12-20 10:03:53 +0100

    poc

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ readme = open(join(curdir, 'README.rst')).read()
 
 setup(
     name             = 'teamux',
-    version          = '0.20.51.0',
+    version          = '0.20.51.1',
     description      = 'Tmux library',
     long_description = readme,
     keywords         = ['utility', ],
